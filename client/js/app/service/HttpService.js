a428e8bcae53aa254fdaca9888cf52ec4a3f8275
Andrewmat <andre.m.santos1@gmail.com>
2017-08-16 16:17:11 -0300

    Removing unnecessary code

--- client/js/app/service/HttpService.js
+++ client/js/app/service/HttpService.js
@@ -1,21 +1,8 @@
 'use strict';
 
 class HttpService {
-
   constructor() {
     throw new Error('HttpService should not be instantiated');
-  }
-
-  static _readyStateChangeHandler(xhr, resolve, reject) {
-    return () => {
-      if (xhr.readyState === 4) {
-        if (xhr.status === 200) {
-          resolve(xhr.responseText);
-        } else {
-          reject(xhr.responseText);
-        }
-      }
-    };
   }
 
   static get(url) {
@@ -33,13 +20,6 @@ class HttpService {
       body: JSON.stringify(data)
     })
       .then(resp => this._handleError(resp));
-    // let xhr = new XMLHttpRequest();
-    // return new Promise((resolve, reject) => {
-    //   xhr.open('POST', url);
-    //   xhr.setRequestHeader('Content-Type', 'application/json');
-    //   xhr.onreadystatechange = HttpService._readyStateChangeHandler(xhr, resolve, reject);
-    //   xhr.send(typeof data === 'string' ? data : JSON.stringify(data));
-    // });
   }
 
   static _handleError(resp) {
